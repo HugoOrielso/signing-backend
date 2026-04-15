664e5e06e9705b0e2efeabc93e801d520d38e727
Hugo Orielso <orielso.lozano15@gmail.com>
2026-04-15 01:19:13 -0500

    updated cors origins

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://theaceous-indorsable-lilliana.ngrok-free.dev",
+  "https://hugoorielso.com",
 ];
 
 const corsOptions: CorsOptions = {
